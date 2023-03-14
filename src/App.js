49b7722b24993f2302dbd7b92e5506689dc19ebe
Kyle Thornton <kyletwebdev@gmail.com>
2023-03-13 17:08:08 -0700

    Starting fresh(ish)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Arrows from './components/Arrows/Arrows';
-import PostCard from './components/PostCard/PostCard';
+
 
 function App() {
   return (
     <div>
       <h1>Reddit</h1>
-      <button>Click on me!</button>
-      <Arrows />
-      <PostCard />
     </div>
   );
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-export default configureStore()
+export const store = configureStore({
+    reducer: {
+        
+    },
+})
