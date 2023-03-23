f4c60545f238c0e22c4e05b4729da02b61f35960
Kyle Thornton <kyletwebdev@gmail.com>
2023-03-23 15:06:01 -0700

    subtitle and slight style imporvements

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   return (
     <div className='pageWrapper'>
       <div className='titleWrapper'>
-        <h1>Reddit</h1>
+        <h1>just a little reddit</h1>
+        <h4>Don't scroll all day. Select how many posts you'd like to see and enjoy reddit...but just a little.</h4>
       </div>
       <PostFeed />
     </div>
