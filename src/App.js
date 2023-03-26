1a9eb2f2dee98e47902077f370743868e07a216d
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-03-26 14:36:42 -0700

    Formatted pics, vids, posts, and links

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <div className='titleWrapper'>
         <h1>just a little reddit</h1>
         <h4>Don't scroll all day. Select how many posts you'd like to see and enjoy reddit...but just a little.</h4>
+        <h5>Note: due to a limitation of the reddit API, video will play without audio.</h5>
       </div>
       <PostFeed />
     </div>
--- src/features/PostFeed.js
+++ src/features/PostFeed.js
@@ -6,8 +6,8 @@ import './PostFeed.css'
 export function PostFeed() {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.postFeed.posts);
-  const status = useSelector((state) => state.postFeed.status);
-  const error = useSelector((state) => state.postFeed.error);
+  // const status = useSelector((state) => state.postFeed.status);
+  // const error = useSelector((state) => state.postFeed.error);
 
   useEffect(() => {
     dispatch(fetchRedditData());
@@ -32,15 +32,15 @@ export function PostFeed() {
           >
             Less
           </button>
-          <button onClick={() => dispatch(fetchRedditData())}>
-            Fetch Data
-          </button>
         </div>
         <div className='feed'>
           {posts.map((post) => (
             <div key={post.id} className='postBox'>
               <h2>{post.title}</h2>
-              <p>{post.selftext}</p>
+              {post.selftext ? <p>{post.selftext}</p> : null}
+              {post.is_video ? <video src={post.secure_media.reddit_video.fallback_url} controls></video> : null}
+              {post.post_hint === "image" ? <img src={post.url_overridden_by_dest}></img> : null}
+              {post.post_hint === "link" ? <a href={post.url_overridden_by_dest}>Link to article</a> : null}
               <h3>{post.subreddit_name_prefixed}</h3>
             </div>
           ))}
