6f25c447fb949dcc8b04fa145106b2a4d2349222
Kyle Thornton <kylemthornton@gmail.com>
2023-11-16 21:23:37 -0800

    Change text size of description

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className='pageWrapper'>
       <div className='titleWrapper'>
         <h1>just a little reddit</h1>
-        <h4>Don't scroll all day. Select how many posts you'd like to see and enjoy reddit...but just a little.</h4>
+        <h3>Don't scroll all day. Select how many posts you'd like to see and enjoy reddit...but just a little.</h3>
         <h5>Note: due to a limitation of the reddit API, video will play without audio.</h5>
         <h5>Also Note: AdBlockers may affect the functionality of this site.</h5>
       </div>
